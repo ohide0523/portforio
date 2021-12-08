08a786741662a07361d3f3cedec05c52213bd4e3
ohide0523 <nmkhx44342@yahoo.co.jp>
2021-12-08 19:25:45 +0900

    fix:絞り込み機能の修正・item追加機能の修正・全item取得機能の修正・item削除機能の修正　create:匿名ログイン機能の追加

---
+++ .history/pages/Login_20211208182839.js
@@ -0,0 +1,267 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useState, useContext, useEffect } from "react";
+import { Context } from "../components/Context";
+import Image from "next/image";
+
+// firebase関連のimport
+import { auth, db } from "../components/firebase";
+import firebase from "@firebase/app";
+
+// header
+import Switch from "@mui/material/Switch";
+import InputBase from "@mui/material/InputBase";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+
+// main
+import { Paper } from "@mui/material";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+import Tooltip from "@mui/material/Tooltip";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+  const { isLogin, setIsLogin, isSearch, setIsSearch, googleLogin, logout } =
+    useContext(Context);
+
+  // 匿名ログイン
+  const anonymouslyLogin = () => {
+    auth()
+      .signInAnonymously()
+      .then(() => {
+        router.push("/Top");
+      });
+  };
+
+  // 検索欄表示の状態管理
+  const onClickSearch = () => {
+    if (isSearch == false) {
+      setIsSearch(true);
+    }
+    if (isSearch == true) {
+      setIsSearch(false);
+    }
+  };
+
+  //emailログイン
+  const emailLogin = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        alert("ログインに成功しました！");
+        setIsLogin(true);
+        setEmail("");
+        setPassword("");
+      })
+      .catch(() => {
+        alert("ログインに失敗しました。。");
+      });
+  };
+
+  // emailアカウント作成
+  const createEmailUser = () => {
+    auth.createUserWithEmailAndPassword(email, password).then((res) => {
+      const user = res.user;
+      db.collection("users").doc(user.uid).set({
+        name: "emailユーザー",
+        id: user.uid,
+        email: "設定されていません",
+        password: password,
+        img: "設定されていません",
+        imgID: "設定されていません",
+        sex: "設定されていません",
+        followerCount: 0,
+        createAt: firebase.firestore.FieldValue.serverTimestamp(),
+      });
+      alert("アカウントを作成しました！");
+      setIsLogin(true);
+      setEmail("");
+      setPassword("");
+    });
+  };
+
+  return (
+    <>
+      {isSearch ? (
+        <>
+          <Tooltip title="戻る" arrow>
+            <ArrowBackIosIcon
+              style={{
+                position: "fixed",
+                top: "20",
+                left: "20",
+                cursor: "pointer",
+              }}
+              onClick={onClickSearch}
+            />
+          </Tooltip>
+
+          <div style={{ alignItems: "center", margin: "auto" }}>
+            <Paper
+              component="form"
+              sx={{
+                p: "2px 4px",
+                display: "flex",
+
+                margin: "auto",
+                marginTop: "300px",
+                width: 400,
+              }}
+            >
+              <IconButton sx={{ p: "10px" }} aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+              <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="里親を探す"
+                inputProps={{ "aria-label": "里親を探す" }}
+              />
+              <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+                <SearchIcon />
+              </IconButton>
+
+              <IconButton
+                color="primary"
+                sx={{ p: "10px" }}
+                aria-label="directions"
+              ></IconButton>
+            </Paper>
+          </div>
+        </>
+      ) : (
+        <>
+          <header
+            style={{
+              display: "flex",
+              alignItems: "center",
+
+              position: "fixed",
+              width: "100%",
+              height: "80px",
+              top: 0,
+              zIndex: 2,
+              background: "white",
+              borderBottom: "1px solid rgb(229, 229, 229)",
+              marginBottom: "100px",
+              background: "#f4f4f4",
+            }}
+          >
+            <h1 style={{ flex: 1, marginLeft: "20px", textAlign: "center" }}>
+              MyPets
+            </h1>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginRight: "10px",
+              }}
+            >
+              <SearchIcon
+                onClick={onClickSearch}
+                style={{ fontSize: "40px", marginRight: "10px" }}
+              />
+              {isLogin ? (
+                <Switch
+                  onChange={logout}
+                  checked={true}
+                  inputProps={{ "aria-label": "controlled" }}
+                />
+              ) : (
+                <Switch
+                  onChange={googleLogin}
+                  checked={false}
+                  inputProps={{ "aria-label": "controlled" }}
+                />
+              )}
+            </div>
+          </header>
+
+          <main
+            style={{
+              width: "100%",
+              margin: "auto",
+              marginTop: "100px",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <h1>ログインページ</h1>
+            <div style={{ textAlign: "center" }}>
+              <h2 onClick={googleLogin}>Googleログイン</h2>
+              <Image
+                src="/googleアイコン.jpeg"
+                width="50px"
+                height="50px"
+                style={{ backGround: "white", cursor: "pointer" }}
+                onClick={googleLogin}
+              />
+              <div style={{ display: "flex", flexDirection: "column" }}>
+                <h2>Emailログインはこちら</h2>
+
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                {/* <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch'},
+      }}
+      noValidate
+      autoComplete="off"
+    >
+<TextField
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+        />
+        </Box> */}
+                <button onClick={emailLogin}>ログイン</button>
+              </div>
+
+              <h3>または</h3>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  marginBottom: "100px",
+                }}
+              >
+                <h2>新規会員登録</h2>
+                <input
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <button onClick={createEmailUser}>新規登録</button>
+              </div>
+                <Button variant="contained">テスト</Button>
+            </div>
+          </main>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Login;
---
+++ .history/pages/[another]/anotherItem/[item]_20211208172105.js
@@ -0,0 +1,239 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useContext,useEffect } from "react";
+import { Context } from "../../../components/Context";
+import firebase from "@firebase/app";
+
+// いいねアイコン
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { db } from "../../../components/firebase";
+
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import { margin } from "@mui/system";
+
+const item = () => {
+  const { like, uid, anotherUser, anotherItem, item} =
+    useContext(Context);
+  const router = useRouter();
+  const {another,item} = router.query
+
+  useEffect(()=>{
+    if (another && item) {
+      // 他のユーザーの情報を取得
+      getAnotherUser(another);
+      // 他のユーザーの持っている特定のitemの取得
+      getAnotherItem(an);
+    }
+  },[another,])
+
+
+  //  いいねする機能
+  const addLikedItem = async (userId, itemId, img, title, count) => {
+    // batch
+    const batch = db.batch();
+
+    const anotherItemRef = db
+      .collection("users")
+      .doc(userId)
+      .collection("items")
+      .doc(itemId);
+    const myUserRef = db.collection("users").doc(uid);
+
+    batch.set(db.doc(anotherItemRef.path).collection("likedUsers").doc(item), {
+      user: uid,
+      itemId: item,
+    });
+
+    batch.update(anotherItemRef, {
+      likeCount: firebase.firestore.FieldValue.increment(1),
+      createAt: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    batch.set(db.doc(myUserRef.path).collection("likedItems").doc(item), {
+      title: title,
+      img: img, //ここに個々のitemの写真を打ち込みたい
+      id: itemId,
+      userId: userId,
+      itemRef: anotherItemRef,
+      like: true, //ここは、likeの状態を表すもの。後で、likeのstateで管理する。
+      likeCount: count,
+    });
+
+    batch.update(myUserRef, {
+      createAt: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    await batch.commit();
+  };
+
+  //   いいねを解除する機能
+  const deleteLikedItem = async (userId, itemId) => {
+    const batch = db.batch();
+    const anotherItemRef = db
+      .collection("users")
+      .doc(userId)
+      .collection("items")
+      .doc(itemId);
+    const myUserRef = db.collection("users").doc(uid);
+    batch.delete(
+      db.doc(anotherItemRef.path).collection("likedUsers").doc(item)
+    );
+    batch.update(anotherItemRef, {
+      likeCount: firebase.firestore.FieldValue.increment(-1),
+    });
+
+    batch.delete(db.doc(myUserRef.path).collection("likedItems").doc(item));
+
+    await batch.commit();
+  };
+
+  // 相手のプロフィールに遷移する
+  const onCLickProfile = (id) => {
+    router.push(`/${id}/profile`);
+  };
+
+  return (
+    <div style={{ width: "80%", margin: "auto" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {anotherItem.length > 0 &&
+          anotherItem.map((item, index) => (
+            <div key={index}>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  padding: "20px",
+                }}
+              >
+                <h1 style={{ fontSize: "23px" }}>ワンちゃんの紹介ページ</h1>
+                {another == uid && (
+                  <Link
+                    href="/myTool/edit/[editItem]"
+                    as={`/myTool/edit/${item.id}`}
+                  >
+                    <Button
+                      variant="outlined"
+                      component="span"
+                      sx={{ height: "30px" }}
+                    >
+                      ワンちゃんの情報を編集する
+                    </Button>
+                  </Link>
+                )}
+                {/* <div>{item.userId}</div> */}
+              </div>
+
+              <img src={item.img} style={{ width: "100%", margin: "auto" }} />
+
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  marginBottom: "30px",
+                }}
+              >
+                <h2 style={{ fontSize: "20px" }}>{item.title}</h2>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  {like ? (
+                    <FavoriteIcon
+                      onClick={() => deleteLikedItem(item.userId, item.id)}
+                    />
+                  ) : (
+                    <FavoriteBorderIcon
+                      onClick={() =>
+                        addLikedItem(
+                          item.userId,
+                          item.id,
+                          item.img,
+                          item.title,
+                          item.likeCount
+                        )
+                      }
+                    />
+                  )}
+                  {item.likeCount}
+                </div>
+              </div>
+
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                <h3>カテゴリー</h3>
+                <p>
+                  {item.category[0]}/{item.category[1]}
+                </p>
+              </div>
+
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  height: "15px",
+                  marginBottom: "40px",
+                }}
+              >
+                <h3>年齢</h3>
+                <p>{item.age}歳</p>
+              </div>
+
+              <div style={{ marginBottom: "40px" }}>
+                <h3>性格・特徴など</h3>
+                <hr />
+
+                <p>{item.content}</p>
+              </div>
+            </div>
+          ))}
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <h2>このワンちゃんの飼い主</h2>
+          <hr />
+          <div>
+            {anotherUser.length > 0 &&
+              anotherUser.map((user, index) => (
+                <div key={index} style={{ marginBottom: "100px" }}>
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      height: "60px",
+                    }}
+                    onClick={() => onCLickProfile(user.id)}
+                  >
+                    <Avatar
+                      alt="アバター画像"
+                      src={user.img}
+                      sx={{ width: 70, height: 70, margin: "20px" }}
+                    />
+                    <div style={{ display: "flex", flexDirection: "column" }}>
+                      <div style={{ fontSize: "20px" }}>{user.name}</div>
+                      <div>
+                        {user.follower}
+                        <span style={{ fontSize: "10px" }}>フォロワー</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default item;
